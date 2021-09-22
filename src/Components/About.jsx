dd7688b082601303d8f8937f1ef2d53b4732ba8b
HaydenDyer <hcdyer0@gmail.com>
2021-09-22 18:15:27 -0400

    adjust CSS for mobile

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,7 +4,7 @@ function About(props) {
     return (
         <div>
             <p>
-                This is an app that utilizes Last.fm's API to show you the current week's most viral music sorted by popularity, so you can keep up with your cool friends and younger colleagues. Grab a pair of headphones and start on your journey to being the hippest person in your social circle!
+                This is an app that utilizes Last.fm's API to show you the past week's most viral music sorted by popularity, so you can keep up with your cooler friends and younger colleagues. Grab a pair of headphones and start on your journey to becoming the hippest person in your social circle!
             </p>
         </div>
     );
