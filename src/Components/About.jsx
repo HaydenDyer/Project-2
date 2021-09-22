163ececc786a25d78fdf96db29b9e6139316d662
HaydenDyer <hcdyer0@gmail.com>
2021-09-22 14:10:20 -0400

    add _redirects and .env.local

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,7 +4,7 @@ function About(props) {
     return (
         <div>
             <p>
-                This is an app that utilizes Last.fm to show you the current week's most popular music, so you can keep up with your hip friends.
+                This is an app that utilizes Last.fm's API to show you the current week's most popular music, so you can keep up with your hip friends. Take a look!
             </p>
         </div>
     );
