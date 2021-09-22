1595047c7ffa488c92f78c3e7c09e2dcc3bda7de
HaydenDyer <hcdyer0@gmail.com>
2021-09-22 16:29:02 -0400

    tweak about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,7 +4,7 @@ function About(props) {
     return (
         <div>
             <p>
-                This is an app that utilizes Last.fm's API to show you the current week's most popular music, so you can keep up with your hip friends. Take a look!
+                This is an app that utilizes Last.fm's API to show you the current week's most viral music sorted by popularity, so you can keep up with your cool friends and younger colleagues. Grab a pair of headphones and start on your journey to being the hippest person in your social circle!
             </p>
         </div>
     );
